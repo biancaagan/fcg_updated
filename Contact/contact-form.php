c9bc5dd69d446e30e1481f79067ed3bfe879fef3
biancaagan <90641547+biancaagan@users.noreply.github.com>
2024-08-09 12:57:42 -0700

    update

--- Contact/contact-form.php
+++ Contact/contact-form.php
@@ -1,5 +1,34 @@
 <?php
 
+// Get data from form  
+$name = $_POST['fullname']; // required
+$email = $_POST['email'];  // required
+$message = $_POST['message'];  // required
+
+$to = "bianca@frequencycoordinationgroup.com";
+$subject = "FCG - New contact form submission";
+
+// The following text will be sent
+// Name = user entered name
+// Email = user entered email
+// Message = user entered message 
+$txt ="Name = ". $name . "\r\n  Email = " 
+    . $email . "\r\n Message =" . $message;
+
+$headers = "From: noreply@demosite.com" . "\r\n" .
+            "CC: somebodyelse@example.com";
+if($email != NULL) {
+    mail($to, $subject, $txt, $headers);
+}
+
+// Redirect to
+header("Location:last.html");
+// ------------------------------
+
+
+
+
+
 if (isset($_POST['email'])) {
      $email_to = "bianca@frequencycoordinationgroup.com";
      $email_subject = "FCG - New contact form submission";
